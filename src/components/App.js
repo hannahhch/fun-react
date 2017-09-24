5287b3e8d219ed21e4360bb0ad50b0e0bee3c310
Hannah Hudson <hannah.hch@gmail.com>
2017-09-24 15:27:25 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import BaseLayout from '../components/BaseLayout.js';
+import ChildComponent from '../components/ChildComponent.js';
+import DisplayComponent from '../components/DisplayComponent.js';
+import Footer from '../components/Footer.js';
+import Header from '../components/Header.js';
+import ParentComponent from '../components/ParentComponent.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BaseLayout></BaseLayout>
+        <Header />
+        <ParentComponent />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
